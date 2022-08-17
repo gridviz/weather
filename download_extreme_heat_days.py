74b6ed7b048fa653616d2e9cd879666a1b40420e
Matt Stiles <mattstiles@gmail.com>
2022-08-17 09:55:13 -0700

    latest

--- download_extreme_heat_days.py
+++ download_extreme_heat_days.py
@@ -9,15 +9,25 @@ with urllib.request.urlopen(json_url) as url:
     data = json.loads(url.read().decode())
 
 
-df = pd.DataFrame(
+src = pd.DataFrame(
     {
         "date": data["features"][0]["attributes"]["Timeseries_Date"].split(","),
         "total": data["features"][0]["attributes"]["Timeseries_Data"].split(","),
     }
 )
 
+src["date"] = pd.to_datetime(src["date"]).dt.strftime('%Y-%m-%d')
+
+df_start = pd.read_csv('data/processed/people_in_excessive_heat_historic.csv')
+
+df_start["date"] = pd.to_datetime(df_start["date"]).dt.strftime('%Y-%m-%d')
+
+df = pd.concat([src, df_start]).drop_duplicates(subset='date').sort_values('date', ascending=False).reset_index(drop=True)
+
 df["date"] = pd.to_datetime(df["date"])
 df["total"] = df["total"].astype(int)
+
+df['pop_share'] = ((df['total'] / 331449281)*100).round(2)
 
 df.to_csv(f"data/processed/people_in_excessive_heat_{today}.csv", index=False)
 df.to_csv(f"data/processed/people_in_excessive_heat_latest.csv", index=False)
@@ -26,3 +36,4 @@ df['date']= df['date'].astype(str)
 
 df.to_json(f"data/processed/people_in_excessive_heat_{today}.json", indent=4, orient='records')
 df.to_json(f"data/processed/people_in_excessive_heat_latest.json", indent=4, orient='records')
+df.to_csv(f"data/processed/people_in_excessive_heat_historic.csv", index=False)
